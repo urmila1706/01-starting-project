f0e8736766f619fb372fa24ecf04e2a26dc30a65
urmila1706 <raut.urmila88@gmail.com>
2024-03-13 22:49:59 +0530

    Add a form component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import React from 'react';
+import AddUser from "./Components/Users/AddUsers";
+//import "./App.css";
 
 
 function App() {
   return (
     <div>
-
+<AddUser/>
     </div>
   );
 }
